6bbbaba6b76b2a760c8e5de6ec9973058e09a087
David Bernazal <d.bernazal@gmail.com>
2014-09-13 10:55:46 -0500

    3. DOM manipulation
    Note the refactor of NavView.
    Open up the console and add/remove element to main container
    Add ContainerView with method to show view in itself
    Add methods to render template and remove the itself from the dom(close)
    Add method to show home view
    Add new binding for home view
    Add function to unbind listeners
    Add close method in NavView to unbindListeners

--- js/app.js
+++ js/app.js
@@ -1,1 +1,46 @@
 //Initialization
+$(document).ready(function() {
+    var navView = new NavView('#navBar');
+});
+
+/****************** Nav View ********************/
+function NavView(elementSelector) {
+    var self = this;
+
+    self.links = {
+        searchLink: $('#navSearch'),
+        homeLink: $('#navHome')
+    }
+
+    self.el = $(elementSelector);
+
+    self.bindListeners();
+}
+
+$.extend(NavView.prototype, {
+    highlightNavLink: function (element) {
+        this.el.find('li').removeClass('active');
+        $(element).addClass('active');
+    },
+
+    bindListeners: function () {
+        var self = this;
+
+        self.el.find('li').click(function() {
+            self.highlightNavLink(this);
+        });
+    }
+});
+
+/****************** Home View ********************/
+function HomeView() {
+    this.template = 
+        '<div class="jumbotron">' +
+            '<h1>Hello, everyone!</h1>' +
+            '<p>This is my Spotify clone, PROSify!</p>' +
+        '</div>';
+}
+
+$.extend(HomeView.prototype, {
+    
+});
